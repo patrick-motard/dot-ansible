47160c364cb54f7b15d770504f439a9a32b52a95
patrick-motard <motard19@gmail.com>
2019-04-15 22:41:45 -0500

    optionally download images from my dropbox by adding download-images tag

--- roles/desktop-background/templates/update-background.sh
+++ roles/desktop-background/templates/update-background.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 DISPLAY=":0"
-notify-send cronjob-message "{{background_images_dir}}"
 files=({{background_images_dir}})
 {% raw %}
 WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
