cc2843b59ebb885e4297f924907ba5a83f801a4b
patrick-motard <motard19@gmail.com>
2019-04-15 22:50:28 -0500

    path to images now is path without glob pattern

--- roles/desktop-background/templates/update-background.sh
+++ roles/desktop-background/templates/update-background.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DISPLAY=":0"
-files=({{background_images_dir}})
+files=({{background_images_dir}}/*)
 {% raw %}
 WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 WALLPAPER2=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
